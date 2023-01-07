dd89add2c6041d1f710ca0d111ac8406b2083eae
Ekatmoz <ekatmoz@gmail.com>
2023-01-07 23:24:14 +0100

    contact componenet

--- src/Componenets/Contact.js
+++ src/Componenets/Contact.js
@@ -1,6 +1,22 @@
 const Contact = () => {
-  return ( <div>
-    <h1>Contacts</h1>
+  return ( <div className="flex">
+     <div>
+      <h2><span>Our </span>contacts:</h2>
+      <p>
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+        </svg>+37 89 4567734</p>
+        <p><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill" viewBox="0 0 16 16">
+  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+</svg>ekatmoz@gmail.com</p>
+    </div>
+    <div class="mapouter">
+      <div class="gmap_canvas">
+        <iframe title="myFrame" width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=si%C3%B3fok,%20szecsenyi%20street%2018&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://putlocker-is.org">.</a>
+          <a href="https://www.embedgooglemap.net">.</a>
+          </div>
+          </div>
+   
   </div> );
 }
  
