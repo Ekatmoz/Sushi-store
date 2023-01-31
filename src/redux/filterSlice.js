d63d141232f24ef9d05a4fbaeb52c0aa08ec15b1
Ekatmoz <ekatmoz@gmail.com>
2023-01-31 12:11:59 +0100

    quantity button

--- src/redux/filterSlice.js
+++ src/redux/filterSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const productSlice = createSlice({
   name: 'product',
   initialState: {
-    selectedCategory: "Pasta"
+    selectedCategory: "All"
   },
   reducers: {
     filterCategory: (state, action) => {
